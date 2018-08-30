17837c14cedf456266b3665abce999819b0ef119
Rocha <jrocha@proforma.com>
2018-08-30 14:41:48 -0400

    More_setup

--- movieapi/src/App.js
+++ movieapi/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch("")
+    .then(resp => {
+      if (resp.status === 200){
+        return resp.json()
+      } else {
+        //handle the error somehow
+      }
+    })
+    .then(json => {
+      console.log(json)
+    })
+  }
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
+      </Router>
     );
   }
 }
