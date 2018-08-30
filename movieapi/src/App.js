0a1c4d5db65c44a329773285375de6c31efd26dc
Alexis Grisham <alexiskjg16@gmail.com>
2018-08-30 15:05:40 -0400

    moviesstart

--- movieapi/src/App.js
+++ movieapi/src/App.js
@@ -4,6 +4,10 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 class App extends Component {
 
+ //API KEY {e68c1a67b9b6a0fff17d9ed980ca72cf}
+ //API Read Access Token {yJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjhjMWE2N2I5YjZhMGZmZjE3ZDllZDk4MGNhNzJjZiIsInN1YiI6IjViODgzNmUyYzNhMzY4MWZlYzAxNWUwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6805CbmV7wyHtccC5-Fzm4J9O9s9m6QhZnBDmfufA0c}
+ //Example Request {https://api.themoviedb.org/3/movie/550?api_key=e68c1a67b9b6a0fff17d9ed980ca72cf}
+
   componentDidMount() {
     fetch("")
     .then(resp => {
